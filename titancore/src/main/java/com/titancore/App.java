5c8383785b12f69d40f9c9da4fb6c6fe38f82008
Miguel Angel Ceballos <miguelceballos2112@gmail.com>
2025-12-01 18:07:27 -0500

    Switches were installed

--- titancore/src/main/java/com/titancore/App.java
+++ titancore/src/main/java/com/titancore/App.java
@@ -5,18 +5,36 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         // Variables where data is entered
-        double distance = ingresarReal("Enter the distance in km: ");
-        int time = ingresarEntero("Enter the time in minutes: ");
-        double initialCapital = ingresarReal("Enter the initial capital: ");
-        double annualInterestRate = ingresarReal("Enter the annual interest rate: ");
-        int capitalizationFrequency = ingresarEntero("Enter the capitalization frequency: ");
-        int years = ingresarEntero("Enter the number of years: ");
-        double pounds = ingresarReal("Enter the number of pounds: ");
-        double pace = calculatePace(distance, time);
-        double compoundInterest = calculateCompoundInterest(initialCapital, annualInterestRate, capitalizationFrequency,
-                years);
-        double kilograms = convertPoundsToKilograms(pounds);
-        mostrarMensaje(pace, compoundInterest, kilograms);
+        int optionCalculator = ingresarEntero(
+                "Ingrese que calculo quiere hace el dia de hoy 1 (Ritmo), 2 (Interes compuesto), 3 (Conversion de libras a kilogramos): ");
+        String mostrarMensaje = "";
+        switch (optionCalculator) {
+            case 1:
+                double distance = ingresarReal("Ingrese la distacia en km: ");
+                int time = ingresarEntero("Ingrese el tiempo en minutos: ");
+                double pace = calculatePace(distance, time);
+                mostrarMensaje = "El ritmo es: " + pace;
+                break;
+            case 2:
+                double initialCapital = ingresarReal("Ingrese el capital inicial: ");
+                double annualInterestRate = ingresarReal("Ingrese la tasa de interes anual: ");
+                int capitalizationFrequency = ingresarEntero("Ingrese la frecuencia de capitalizacion: ");
+                int years = ingresarEntero("Ingrese el numero de años: ");
+                double compoundInterest = calculateCompoundInterest(initialCapital, annualInterestRate,
+                        capitalizationFrequency, years);
+                mostrarMensaje = "El interes compuesto es: " + compoundInterest;
+                break;
+            case 3:
+                double pounds = ingresarReal("Ingrese el numero de libras: ");
+                double kilograms = convertPoundsToKilograms(pounds);
+                mostrarMensaje = "El peso en kilogramos es: " + kilograms;
+                break;
+            default:
+                mostrarMensaje = "Opcion no valida";
+                break;
+        }
+        System.out.println(mostrarMensaje);
+
     }
 
     // Converter variables and calculators
@@ -53,14 +71,4 @@ public class App {
         return valorEntero;
     }
 
-    public static void mostrarMensaje(String mensaje) {
-        System.out.println(mensaje);
-
-    }
-
-    public static void mostrarMensaje(double pace, double compoundInterest, double kilograms) {
-        System.out.println("The average pace according to the data entered is: " + pace +
-                "\nThe interest you would be receiving, according to the data entered, is: " +
-                compoundInterest + "\nThe weight in kilograms is: " + kilograms);
-    }
 }
