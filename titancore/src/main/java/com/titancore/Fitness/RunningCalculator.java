7815b3d8d5064f3ff19e644f31d2b46bdbf22c94
Miguel Angel Ceballos <miguelceballos2112@gmail.com>
2025-12-09 21:56:52 -0500

    Revert "Creating a submenu and list to store running paces"
    This reverts commit d0ab2146cd122e6b9809f7a89d7f803836ebe65f.

--- titancore/src/main/java/com/titancore/Fitness/RunningCalculator.java
+++ titancore/src/main/java/com/titancore/Fitness/RunningCalculator.java
@@ -1,24 +1,9 @@
 package com.titancore.Fitness;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class RunningCalculator {
-    public List<Double> paceHistory = new ArrayList<>();
-
     public double calculatePace(double distance, int time) {
         double pace = Math.round(time / distance * 100.0) / 100.0;
         return pace;
     }
-
-    public void addPaceToHistory(double pace) {
-        paceHistory.add(pace);
-    }
-
-    public List<Double> getPaceHistory() {
-        return paceHistory;
-    }
-
-
 
 }
